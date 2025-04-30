de99545ca047931d1ee6c4df71fd8b274c39f6b6
MaxDevcic <ma.devcic@duocuc.cl>
2025-04-30 12:36:12 -0400

    first commit

---
+++ src/main/java/com/ejemplotravez/mascotas_3/Mascotas3Application.java
@@ -0,0 +1,13 @@
+package com.ejemplotravez.mascotas_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mascotas3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mascotas3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ejemplotravez/mascotas_3/Mascotas3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejemplotravez.mascotas_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mascotas3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
